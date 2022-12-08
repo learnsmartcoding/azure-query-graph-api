5ed0f23f001d744c6f282d3e3495bf2a9a227023
Learn Smart Coding <LearnSmartCoding@gmail.com>
2022-12-08 17:01:30 -0500

    working code sample

---
+++ QueryGraphAPI/Models/UsersModel.cs
@@ -0,0 +1,17 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Newtonsoft.Json;
+
+namespace QueryGraphAPI
+{
+    public class UsersModel
+    {
+        public UserModel[] Users { get; set; }
+
+        public static UsersModel Parse(string JSON)
+        {
+            return JsonConvert.DeserializeObject(JSON, typeof(UsersModel)) as UsersModel;
+        }
+    }
+}
